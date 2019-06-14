8ac9b6432a27fd8ba306cf1e18dd8b63fa8f1418
zhaiyujia <zhaiyujia0828@163.com>
2019-06-14 08:45:56 +0800

    feat myself sb

---
+++ src/com/leetcode/middleNode/ListNode.java
@@ -0,0 +1,17 @@
+package com.leetcode.middleNode;
+
+/**
+ * @author zhaiyj
+ * @date 2019/6/13 8:56
+ * @instruction
+ */
+
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
