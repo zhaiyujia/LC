a5674bb9a6222c04b190796417f860d112c5dd85
zhaiyujia <zhaiyujia0828@163.com>
2019-06-14 09:31:49 +0800

    feat myself sb

---
+++ src/com/leetcode/levelOrderBottom/TreeNode.java
@@ -0,0 +1,17 @@
+package com.leetcode.levelOrderBottom;
+
+/**
+ * @author zhaiyj
+ * @date 2019/6/14 8:47
+ * @instruction
+ */
+public class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){
+		val = x;
+	}
+
+}
