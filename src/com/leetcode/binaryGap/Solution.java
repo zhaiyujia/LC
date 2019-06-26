11a3cc3651e986e7f3f5f055c68f6e4003dff9a1
zhaiyujia <zhaiyujia0828@163.com>
2019-06-26 09:04:13 +0800

    feat evloution

--- src/com/leetcode/binaryGap/Solution.java
+++ src/com/leetcode/binaryGap/Solution.java
@@ -14,40 +14,67 @@ import java.util.List;
 public class Solution {
 
 	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(22));
 
-		int n = 8;
-		int res = binaryGap(n);
-		System.out.println(res);
+//		int n = 22;
+//		int res = binaryGap(n);
+//		System.out.println(res);
 	}
 
 	public static int binaryGap(int N) {
-		String str = Integer.toBinaryString(N);
-		// 不包含1的，直接返回0
-		if(str.indexOf("1")==-1){
+
+		if(N<2){
 			return 0;
 		}
-		if(str.indexOf("1") == str.lastIndexOf("1")){
-			return 0;
-		}
-		List<Integer> sortList = new ArrayList<>();
-		int index = str.indexOf("1");
-		char c = str.charAt(index);
-		int count = 0;
-		int size = str.length();
-		for (int i = index+1; i < size; i++) {
-			char x = str.charAt(i);
-			if(x == c){
-				int distance = i-index;
-				if(distance>count){
-					count = distance;
+
+		int right = 0;
+		int left = 0;
+		int idx = 0;
+		int res = 0;
+
+		while (N>0){
+			idx++;
+			if((N&1)==1){
+				int temp = left;
+				left = idx;
+				if(right==0){
+					right = left;
+				}else{
+					right = temp;
 				}
-				index = str.indexOf("1",i);
-				sortList.add(count);
-				count = 0;
+				res = Math.max(res,left-right);
 			}
+			N = N >> 1;
 		}
-		Collections.sort(sortList);
-		return sortList.get(sortList.size()-1);
+		return res;
+
+//		String str = Integer.toBinaryString(N);
+//		// 不包含1的，直接返回0
+//		if(str.indexOf("1")==-1){
+//			return 0;
+//		}
+//		if(str.indexOf("1") == str.lastIndexOf("1")){
+//			return 0;
+//		}
+//		List<Integer> sortList = new ArrayList<>();
+//		int index = str.indexOf("1");
+//		char c = str.charAt(index);
+//		int count = 0;
+//		int size = str.length();
+//		for (int i = index+1; i < size; i++) {
+//			char x = str.charAt(i);
+//			if(x == c){
+//				int distance = i-index;
+//				if(distance>count){
+//					count = distance;
+//				}
+//				index = str.indexOf("1",i);
+//				sortList.add(count);
+//				count = 0;
+//			}
+//		}
+//		Collections.sort(sortList);
+//		return sortList.get(sortList.size()-1);
 	}
 
 }
