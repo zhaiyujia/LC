286e9deeeaf717964317f84995dd31ac8bf16526
zhaiyujia <zhaiyujia0828@163.com>
2019-07-04 09:38:35 +0800

    feat two string find different words less one

---
+++ src/com/leetcode/findTheDifference/Solution.java
@@ -0,0 +1,57 @@
+package com.leetcode.findTheDifference;
+
+import java.util.Arrays;
+
+/**
+ * @author zhaiyj
+ * @date 2019/6/28 9:07
+ * @instruction
+ * 给定两个字符串 s 和 t，它们只包含小写字母。
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * 请找出在 t 中被添加的字母。
+ *
+ *
+ * 输入：
+ * s = "abcd"
+ * t = "abcde"
+ * 输出：
+ * e
+ * 解释：'e' 是那个被添加的字母。
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		String s = "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor";
+		String t = "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj";
+		findTheDifference(s, t);
+	}
+
+	public static char findTheDifference(String s, String t) {
+
+		char[] cs = s.toCharArray();
+		char[] ct = t.toCharArray();
+		int sum1 = 0;
+		for (int i = 0; i < cs.length; i++) {
+			sum1 += cs[i];
+		}
+
+		int sum2 = 0;
+		for (int i = 0; i < ct.length; i++) {
+			sum2 += ct[i];
+		}
+		int diff = sum2 - sum1;
+		char cNumber= (char) (diff);
+		return cNumber;
+//		char[] cs = s.toCharArray();
+//		char[] ct = t.toCharArray();
+//		Arrays.sort(cs);
+//		Arrays.sort(ct);
+//		int i = 0;
+//		for(; i < cs.length; i++) {
+//			if(cs[i] != ct[i]) {
+//				return ct[i];
+//			}
+//		}
+//		return ct[i];
+	}
+}
