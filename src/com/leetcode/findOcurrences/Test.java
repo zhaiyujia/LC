34e079acd80fd214f8f7ec074d5be4b2a8ba66d0
zhaiyujia <zhaiyujia0828@163.com>
2019-07-16 09:35:28 +0800

    LeetCode find Ocurrences

---
+++ src/com/leetcode/findOcurrences/Test.java
@@ -0,0 +1,16 @@
+package com.leetcode.findOcurrences;
+
+/**
+ * @author zhaiyj
+ * @date 2019/7/16 9:25
+ * @description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String str = "[ A d s a d s a";
+        str.split(" ");
+    }
+
+
+}
