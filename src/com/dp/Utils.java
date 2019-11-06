e91171d177038b475a979de46c49937588e90d1c
zhaiyj <zhaiyj@fxiaoke.com>
2019-11-06 09:03:02 +0800

    feat 数组

---
+++ src/com/dp/Utils.java
@@ -0,0 +1,13 @@
+package com.dp;
+
+/**
+ * Created by zhaiyj
+ * date 2019/11/4 9:24 上午
+ */
+public class Utils {
+
+    public static void println(Integer t){
+        System.out.println(t);
+    }
+
+}
