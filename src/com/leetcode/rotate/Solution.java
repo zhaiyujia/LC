b0f9ea61dc16645194b2b5bd437ff5548ff001a3
zhaiyj <zhaiyj@fxiaoke.com>
2021-12-02 10:38:38 +0800

    feat all

--- src/com/leetcode/rotate/Solution.java
+++ src/com/leetcode/rotate/Solution.java
@@ -9,6 +9,23 @@ package com.leetcode.rotate;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        int k = 3;
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
+        System.out.println(nums);
+    }
 
+
+    // [5,6,7,1,2,3,4]
+    public static void rotate(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int value = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = value;
+        }
+    }
 
 }
