59aba3760d07c103c01c362e3f48ea54ed456fad
zhaiyujia02 <zhaiyujia02@58.com>
2023-07-05 15:07:26 +0800

    feat 两个数相加

--- src/com/leetcode/mid/addTwoNumbers/Solution.java
+++ src/com/leetcode/mid/addTwoNumbers/Solution.java
@@ -1,5 +1,10 @@
 package com.leetcode.mid.addTwoNumbers;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * @author zhai
  * @date 2023/7/3 4:10 PM
@@ -23,15 +28,16 @@ public class Solution {
         ListNode l2 = new ListNode(9);
         l2.next = new ListNode(9);
 
-        ListNode listNode = addTwoNumbers(l1, l2);
-
+//        ListNode listNode = addTwoNumbers(l1, l2);
+        addTwo(l1, l2);
     }
 
     public static void test3() {
         ListNode l1 = new ListNode(0);
         ListNode l2 = new ListNode(0);
 
-        ListNode listNode = addTwoNumbers(l1, l2);
+//        ListNode listNode = addTwoNumbers(l1, l2);
+        addTwo(l1, l2);
 
     }
 
@@ -44,7 +50,8 @@ public class Solution {
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
-        ListNode listNode = addTwoNumbers(l1, l2);
+//        ListNode listNode = addTwoNumbers(l1, l2);
+        addTwo(l1, l2);
 
     }
 
@@ -57,7 +64,8 @@ public class Solution {
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
-        ListNode listNode = addTwoNumbers(l1, l2);
+//        ListNode listNode = addTwoNumbers(l1, l2);
+        addTwo(l1, l2);
 
     }
 
@@ -71,6 +79,158 @@ public class Solution {
             prev = current;
         }
         return prev;
+    }
+
+    public static ListNode one(ListNode l1, ListNode l2) {
+        LinkedList<Integer> stack1 = new LinkedList<>();
+        LinkedList<Integer> stack2 = new LinkedList<>();
+
+        while (l1 != null && l2 != null) {
+            stack1.push(l1.val);
+            stack2.push(l2.val);
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode head = null;
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            int result = stack1.poll() + stack2.poll() + carry;
+            carry = result / 10;
+            result = result % 10;
+            ListNode listNode = new ListNode(result);
+            listNode.next = head;
+            head = listNode;
+        }
+
+        while (!stack1.isEmpty()) {
+            int result = stack1.pop() + carry;
+            carry = result / 10;
+            result = result % 10;
+            ListNode listNode = new ListNode(result);
+            listNode.next = head;
+            head = listNode;
+        }
+
+        while (!stack2.isEmpty()) {
+            int result = stack2.pop() + carry;
+            carry = result / 10;
+            result = result % 10;
+            ListNode listNode = new ListNode(result);
+            listNode.next = head;
+            head = listNode;
+        }
+
+        if (carry != 0) {
+            ListNode listNode = new ListNode(carry);
+            listNode.next = head;
+            head = listNode;
+        }
+
+        return head;
+    }
+
+
+    public static ListNode addTwo(ListNode l1, ListNode l2) {
+        Deque<Integer> stack1 = new ArrayDeque<Integer>();
+        Deque<Integer> stack2 = new ArrayDeque<Integer>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        int carry = 0;
+        ListNode ans = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int a = stack1.isEmpty() ? 0 : stack1.pop();
+            int b = stack2.isEmpty() ? 0 : stack2.pop();
+            int cur = a + b + carry;
+            carry = cur / 10;
+            cur %= 10;
+            ListNode curnode = new ListNode(cur);
+            curnode.next = ans;
+            ans = curnode;
+        }
+        return ans;
+    }
+
+    public static ListNode addTwo1(ListNode l1, ListNode l2) {
+        // 都是0的情况下，直接返回某一个节点
+        if (l1.val == 0 && l2.val == 0) {
+            return l1;
+        }
+
+        Stack<Integer> stack1 = new Stack<>();
+        stack1.push(l1.val);
+        ListNode t1 = l1.next;
+        while (t1 != null) {
+            stack1.push(t1.val);
+            t1 = t1.next;
+        }
+
+        Stack<Integer> stack2 = new Stack<>();
+        stack2.push(l2.val);
+        ListNode t2 = l2.next;
+        while (t2 != null) {
+            stack2.push(t2.val);
+            t2 = t2.next;
+        }
+
+        ListNode result = new ListNode();
+        boolean needJW = false;
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            int value1 = 0;
+            try {
+                value1 = stack1.pop();
+            } catch (Exception ignored) {
+                // 出现异常直接按照0算
+            }
+
+            int value2 = 0;
+            try {
+                value2 = stack2.pop();
+            } catch (Exception ignored) {
+                // 出现异常直接按照0算
+            }
+
+            int add = 0;
+            if (needJW) {
+                needJW = false;
+                add = 1;
+            }
+            int sum = value1 + value2 + add;
+            if (sum >= 10) {
+                // 进位
+                needJW = true;
+            }
+            ListNode node = new ListNode(sum % 10);
+            if (result.next != null) {
+                node.next = result.next;
+                result.next = node;
+            } else {
+                result.next = node;
+            }
+        }
+        if (needJW) {
+            result.val = 1;
+            return result;
+        }
+
+
+        return result.next;
     }
 
 
