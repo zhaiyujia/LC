db215cb2d81bcfd8edf7ca45c3418d3ba63ebee3
zhaiyujia02 <zhaiyujia02@58.com>
2023-07-14 10:51:45 +0800

    feat

---
+++ src/com/leetcode/mid/removeDuplicateLetters/Solution.java
@@ -0,0 +1,44 @@
+package com.leetcode.mid.removeDuplicateLetters;
+
+import java.util.*;
+
+/**
+ * @author zhai
+ * @date 2023/7/12 4:05 PM
+ * @illustration
+ * @slogan: Treat others the way you want to be treated
+ * @version:
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        String s = "cbacdcbc";
+        String s1 = removeDuplicateLetters(s);
+        System.out.println(s1);
+    }
+
+
+    public static String removeDuplicateLetters(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (stack.contains(c)) {
+                continue;
+            }
+            while (!stack.isEmpty()
+                    && stack.peek() > c
+                    && s.indexOf(stack.peek(), i) != -1) {
+                stack.pop();
+            }
+
+            stack.push(c);
+        }
+        char chars[] = new char[stack.size()];
+        for (int i = 0; i < stack.size(); i++) {
+            chars[i] = stack.get(i);
+        }
+        return new String(chars);
+    }
+
+
+}
---
+++ src/com/leetcode/mid/threeSum/Solution.java
@@ -0,0 +1,11 @@
+package com.leetcode.mid.threeSum;
+
+/**
+ * @author zhai
+ * @date 2023/7/14 10:51 AM
+ * @illustration
+ * @slogan: Treat others the way you want to be treated
+ * @version:
+ */
+public class Solution {
+}
