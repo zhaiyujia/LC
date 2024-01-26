a889384d294bd33844c7d0b08b8a649908b227e8
zhaiyujia02 <zhaiyujia02@58.com>
2024-01-26 09:56:47 +0800

    feat 移除重复元素

--- src/com/leetcode/boxuegu/rotate/Solution.java
+++ src/com/leetcode/boxuegu/rotate/Solution.java
@@ -34,6 +34,16 @@ public class Solution {
         }
     }
 
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+
+
 
 
     // 超时
