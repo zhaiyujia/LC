5b242ecd3a2c271a54d8a20173e3a2235fd588f0
zhaiyujia02 <zhaiyujia02@58.com>
2024-01-29 21:32:29 +0800

    feat 增加说明信息

--- src/com/lc/boxuegu/fourSum/Solution.java
+++ src/com/lc/boxuegu/fourSum/Solution.java
@@ -2,6 +2,7 @@ package com.lc.boxuegu.fourSum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,14 +19,15 @@ public class Solution {
         Solution solution = new Solution();
         List<List<Integer>> lists = solution.fourSum(nums, 4);
         for (List<Integer> list : lists) {
-            list.forEach(o-> System.out.println(o + ","));
+            list.forEach(o -> System.out.println(o + ","));
             System.out.println("---------------------");
         }
     }
 
+    // 有问题
     public List<List<Integer>> fourSum(int[] nums, int target) {
         if (nums == null || nums.length < 4) {
-            return null;
+            return Collections.EMPTY_LIST;
         }
 
         Arrays.sort(nums);
@@ -41,7 +43,7 @@ public class Solution {
             }
 
             if (nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3] + nums[nums.length - 4] < target) {
-                break;
+                continue;
             }
 
             for (int j = i + 1; j < nums.length - 2; j++) {
@@ -51,8 +53,12 @@ public class Solution {
 
                 int min = nums[i] + nums[j] + nums[j + 1] + nums[j + 2];
                 int max = nums[i] + nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3];
-                if (min > target || max < target) {
+                if (min > target) {
                     break;
+                }
+
+                if (max < target) {
+                    continue;
                 }
 
                 int a = nums[i];
@@ -88,5 +94,59 @@ public class Solution {
         }
         return res;
     }
+
+
+    // 官方题解
+    public List<List<Integer>> fourSum2(int[] nums, int target){
+        List<List<Integer>> quadruplets = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
+            return quadruplets;
+        }
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return quadruplets;
+    }
+
 
 }
