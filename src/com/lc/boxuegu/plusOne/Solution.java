777ac476672280aad756a7e27f602b77246e4417
rk_z <zhaiyujia0828@163.com>
2025-01-03 21:31:10 +0800

    feat 修改jdk21

--- src/com/lc/boxuegu/plusOne/Solution.java
+++ src/com/lc/boxuegu/plusOne/Solution.java
@@ -11,8 +11,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = new int[]{9};
-        int[] ints = solution.plusOne2_1(nums);
+        int[] nums = new int[]{9, 3, 9, 9};
+        int[] ints = solution.plusOne3_2(nums);
         for (int anInt : ints) {
             System.out.println(anInt);
         }
@@ -47,7 +47,7 @@ public class Solution {
             int digit = digits[i];
             digits[i] = (digit + array) % 10;
             array = (digit + array) / 10;
-            if(array == 0){
+            if (array == 0) {
                 break;
             }
         }
@@ -57,6 +57,46 @@ public class Solution {
             return ans;
         }
         return digits;
+    }
+
+    public int[] plusOne3_1(int[] digits) {
+        int temp = 1;
+        int index = digits.length - 1;
+        for (int i = index; i >= 0; i--) {
+
+            int curr = (digits[i] + temp) % 10;
+            temp = (digits[i] + temp) / 10;
+            digits[i] = curr;
+        }
+
+        if (temp == 1) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            for (int i = 1; i < result.length; i++) {
+                result[i] = digits[i - 1];
+            }
+            return result;
+        }
+        return digits;
+    }
+
+
+    public int[] plusOne3_2(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                ++digits[i];
+                for (int j = i + 1; j < n; j++) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+
+        // digits 中所有的元素均为 9
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
+        return ans;
     }
 
 }
