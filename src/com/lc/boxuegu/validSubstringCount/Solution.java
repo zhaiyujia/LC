06335f0edce9ded324be979ab1fa012fe0b026da
zhaiyujia02 <zhaiyujia02@58.com>
2025-01-10 11:21:39 +0800

    3297 leetcode

--- src/com/lc/boxuegu/validSubstringCount/Solution.java
+++ src/com/lc/boxuegu/validSubstringCount/Solution.java
@@ -41,6 +41,37 @@ public class Solution {
 
     }
 
+
+    private static boolean ok(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] < b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long validSubstringCount1_1(String word1, String word2) {
+        int[] cnt1 = new int[26], cnt2 = new int[26];
+        for (char c : word2.toCharArray()) {
+            cnt2[c - 'a']++;
+        }
+        long ans = 0;
+        for (int l = 0, r = 0; l < word1.length(); l++) {
+            if (l > 0) {
+                cnt1[word1.charAt(l - 1) - 'a']--;
+            }
+            while (!ok(cnt1, cnt2)) {
+                if (r == word1.length()) {
+                    return ans;
+                }
+                cnt1[word1.charAt(r++) - 'a']++;
+            }
+            ans += word1.length() - r + 1;
+        }
+        return ans;
+    }
+
     public static long validSubstringCount_ERROR(String word1, String word2) {
         List<String> list = new ArrayList<>();
         int n = word1.length();
