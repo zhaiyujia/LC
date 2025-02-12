443f48a441fd08a0370a65833a008d5f8aebd616
rk_z <zhaiyujia0828@163.com>
2025-02-12 21:37:59 +0800

    1760. 袋子里最少数目的球

---
+++ src/com/lc/boxuegu/M_minimumSize/Solution.java
@@ -0,0 +1,38 @@
+package com.lc.boxuegu.M_minimumSize;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,4,8,2};
+        int maxOperations = 4;
+        Solution solution = new Solution();
+        int i = solution.minimumSize(nums, maxOperations);
+        System.out.println(i);
+    }
+
+    public int minimumSize(int[] nums, int maxOperations) {
+        int l = 1;
+        int r = 0;
+        for (int num : nums) {
+            r = Math.max(num, r);
+        }
+
+        while (l < r){
+            int mid = (l + r) >> 1;
+            int total = 0;
+            for (int num : nums) {
+                total += (num - 1) / mid;
+            }
+            if (total <= maxOperations){
+                r = mid;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+
+
+}
